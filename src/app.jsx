063c69a0081a214b26fdba2ad64ca2bdfa1b67c5
vasilina.m <vasilina.m@ri.domain>
2025-07-28 16:59:05 +0300

    new commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Portfolio from './components/Portfolio';
+import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <h1>Портфолио с фильтрами</h1>
       <Portfolio />
     </div>
   );
