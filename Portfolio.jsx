b58ecb15643b71a48811590655e9338d4dc50a41
vasilina.m <vasilina.m@ri.domain>
2025-07-28 15:52:33 +0300

    new commit

--- Portfolio.jsx
+++ Portfolio.jsx
@@ -1,124 +1,31 @@
-import React, { Component } from 'react';
-
-class Portfolio extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedFilter: 'All',
-      projects: [
-        {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
-          category: "Business Cards"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
-          category: "Business Cards"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-          category: "Business Cards"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
-          category: "Flayers"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
-          category: "Business Cards"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
-          category: "Business Cards"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
-          category: "Websites"
-        }, {
-          img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
-          category: "Flayers"
-        }
-      ]
-    };
+class Portfolio extends React.Component {
+  state = {
+    selected: 'All',
+    projects: [...], // данные из задания
+    filters: ['All', 'Websites', 'Flayers', 'Business Cards']
   }
 
-  handleSelectFilter = (filter) => {
-    this.setState({ selectedFilter: filter });
-  };
+  onSelectFilter = (filter) => {
+    this.setState({ selected: filter });
+  }
 
   getFilteredProjects() {
-    const { selectedFilter, projects } = this.state;
-    if (selectedFilter === 'All') {
-      return projects;
+    if (this.state.selected === 'All') {
+      return this.state.projects;
     }
-    return projects.filter(project => project.category === selectedFilter);
+    return this.state.projects.filter(project => project.category === this.state.selected);
   }
 
   render() {
-    const { selectedFilter } = this.state;
-    const filters = ['All', 'Websites', 'Flayers', 'Business Cards'];
-    const filteredProjects = this.getFilteredProjects();
-
     return (
-      <div className="portfolio">
-        <Toolbar
-          filters={filters}
-          selected={selectedFilter}
-          onSelectFilter={this.handleSelectFilter}
+      <div>
+        <Toolbar 
+          filters={this.state.filters}
+          selected={this.state.selected}
+          onSelectFilter={this.onSelectFilter}
         />
-        <ProjectList projects={filteredProjects} />
+        <ProjectList projects={this.getFilteredProjects()} />
       </div>
     );
   }
 }
-
-const Toolbar = ({ filters, selected, onSelectFilter }) => {
-  return (
-    <div className="toolbar">
-      {filters.map(filter => (
-        <button
-          key={filter}
-          className={filter === selected ? 'active' : ''}
-          onClick={() => onSelectFilter(filter)}
-        >
-          {filter}
-        </button>
-      ))}
-    </div>
-  );
-};
-
-const ProjectList = ({ projects }) => {
-  return (
-    <div className="project-list">
-      {projects.map((project, index) => (
-        <div key={index} className="project">
-          <img src={project.img} alt={project.category} />
-          <div className="category">{project.category}</div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Portfolio;
